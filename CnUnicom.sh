dc9c3355684affca9756ba30c520aa2a91ddecbf
Chrisma <69295068+Chrisma-98@users.noreply.github.com>
2020-08-18 17:04:24 +0800

    Update CnUnicom.sh

--- CnUnicom.sh
+++ CnUnicom.sh
@@ -9,8 +9,8 @@ export PATH
 alias curl='curl -m 10'
 
 # user info: change them to yours or use parameters instead.
-username=""
-password=""
+username="17630631630"
+password="760303633"
 
 # 联通APP版本
 unicom_version=7.0402
